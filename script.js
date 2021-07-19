fd4547d3b1307dab00ad89bedfff49ffb665cbb1
Southern Comfort <scottguenther94@gmail.com>
2021-07-19 17:51:50 -0500

    added editable table cells

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
 console.log("hello");
-var saveButton = document.getElementById("save");
-function savePlan() {
-    // Save related form data as an object
-    var studentGrade = {
-      student: student.value,
-      grade: grade.value,
-      comment: comment.value.trim()
-    };
-    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-    localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
-  }
-  
-  function renderPlan() {
-    // Use JSON.parse() to convert text to JavaScript object
-    var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
-    // Check if data is returned, if not exit out of the function
-    if (lastGrade !== null) {
-    document.getElementById("saved-name").innerHTML = lastGrade.student;
-    document.getElementById("saved-grade").innerHTML = lastGrade.grade;
-    document.getElementById("saved-comment").innerHTML = lastGrade.comment;
-    } else {
-      return;
-    }
-  }
+const editables = document.querySelectorAll("[contenteditable]");
 
+// save edits
+editables.forEach(el => {
+  el.addEventListener("blur", () => {
+    localStorage.setItem("dataStorage-" + el.id, el.innerHTML);
+  })
+});
 
-saveButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    savePlan();
-    renderPlan();
-    });
+// once on load
+for (var key in localStorage) {
+  if (key.includes("dataStorage-")) {
+    const id = key.replace("dataStorage-","");
+    document.querySelector("#" + id).innerHTML = localStorage.getItem(key);
+  }
+}
